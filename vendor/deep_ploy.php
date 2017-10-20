fb6f9bf285bed229934d8b4135aa13179d80bdaf
Tim Tepatti <ttepatti@gmail.com>
2017-10-20 15:36:32 -0400

    Fixed deploy script more
    Last one exposed sensitive info :P

--- vendor/deep_ploy.php
+++ vendor/deep_ploy.php
@@ -8,13 +8,7 @@
 	 */
 	// The commands
 	$commands = array(
-		'echo $PWD',
-		'whoami',
-		'git pull',
-		'git status',
-		'git submodule sync',
-		'git submodule update',
-		'git submodule status',
+		'git pull https://github.com/ttepatti/tepatti.com.git'
 	);
 	// Run the commands for output
 	$output = '';
